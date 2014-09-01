3c948a00fe331915f9718481ee2017dcbb086544
Bobby <steinbach.rj@gmail.com>
2014-09-01 15:13:33 -0400

    NullPointerException fix when trying to compare to contacts and one is null. Version Code bump

--- app/src/main/java/me/iambob/spitly/activities/SendTextActivity.java
+++ app/src/main/java/me/iambob/spitly/activities/SendTextActivity.java
@@ -142,7 +142,11 @@ public class SendTextActivity extends WaitForContactsActivity implements ChooseN
         Collections.sort(loadedContacts, new Comparator<Contact>() {
             @Override
             public int compare(Contact o1, Contact o2) {
-                return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+                try {
+                    return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+                } catch (NullPointerException exc) {
+                    return 1;
+                }
             }
         });
 
